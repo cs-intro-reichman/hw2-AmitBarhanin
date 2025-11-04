70968483b4599e714d42c4e63248fb9669f8134d
amitbarha <amitbar131@gmail.com>
2025-11-04 23:46:37 +0200

    using indexof

--- Cheers.java
+++ Cheers.java
@@ -3,48 +3,15 @@ public class Cheers {
         public static void main(String[] args) {
                 String word = args[0].toUpperCase();
                 int nTimes = Integer.parseInt(args[1]);
-                char[] wordArr = word.toCharArray();
-                for (int i = 0; i < wordArr.length; i++) {
+               // char[] wordArr = word.toCharArray();
+                for (int i = 0; i < word.length(); i++) {
                         String anOrA = "a ";
-                        switch (wordArr[i]) {
-                                case 'A':
-                                        anOrA = "an";
-                                        break;
-                                case 'E':
-                                        anOrA = "an";
-                                        break;
-                                case 'F':
-                                        anOrA = "an";
-                                        break;
-                                case 'H':
-                                        anOrA = "an";
-                                        break;
-                                case 'I':
-                                        anOrA = "an";
-                                        break;
-                                case 'L':
-                                        anOrA = "an";
-                                        break;
-                                case 'M':
-                                        anOrA = "an";
-                                        break;
-                                case 'N':
-                                        anOrA = "an";
-                                        break;
-                                case 'O':
-                                        anOrA = "an";
-                                        break;
-                                case 'R':
-                                        anOrA = "an";
-                                        break;
-                                case 'S':
-                                        anOrA = "an";
-                                        break;
-                                case 'X':
-                                        anOrA = "an";
-                                        break;
-                        }
-                        System.out.println("Give me " + anOrA + " " + wordArr[i] + ": " + wordArr[i] + "!");
+                        String anLetters = "AEFHILMNORSX";
+                        char letter = word.charAt(i);
+                     if (anLetters.indexOf(letter) != -1) {
+                        anOrA = "an";
+                     }
+                        System.out.println("Give me " + anOrA + " " + letter + ": " + letter + "!");
 
                 }
                 System.out.println("What does that spell?");
